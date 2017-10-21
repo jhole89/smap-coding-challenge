55b1c73cfa7fb5a538b925112e4eb6d92443dae5
Joel Lutman <joellutman@gmail.com>
2017-10-21 23:22:23 +0100

    views first pass

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from models import Consumption
+from models import User
 
 # Create your views here.
 
 
 def summary(request):
+
+    all_users = User.objects.all().values('id', 'area', 'tariff')
+    user_data = []
+
+    for user in all_users:
+
+        user_data.append({
+            'id': user.id,
+            'total_consumption': Consumption.get_total(user),
+            'avg_consumption': Consumption.get_average(user),
+        })
+
     context = {
-        'message': 'Hello!',
+        'data': user_data,
     }
     return render(request, 'consumption/summary.html', context)
 
