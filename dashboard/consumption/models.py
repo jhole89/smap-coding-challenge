0c1f0ddc0458c84c1005660d2a8cc97063bb5d27
Joel Lutman <joellutman@gmail.com>
2017-10-21 19:30:26 +0100

    add aggregation methods

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -26,3 +26,27 @@ class Consumption(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
     consumption = models.FloatField()
+
+    def get_total(self, user):
+
+        user_consumptions = Consumption.objects.filter(user_id=user)
+
+        total_consumptions = 0.0
+
+        for consumption_record in user_consumptions:
+            total_consumptions += consumption_record.consumption
+
+        return total_consumptions
+
+    def get_average(self, user):
+
+        user_consumptions = Consumption.objects.filter(user_id=user)
+
+        avg_consumption = 0.0
+
+        for record_count, consumption_record in enumerate(user_consumptions):
+            avg_consumption += consumption_record.consumption
+
+        avg_consumption = avg_consumption/float(record_count)
+
+        return avg_consumption
